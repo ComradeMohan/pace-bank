c41763498e2164c76c2093abc97b0d38353a6f84
Mohan Reddy <129178102+ComradeMohan@users.noreply.github.com>
2025-05-21 19:11:28 +0530

    Update config.php

--- SAMPLE/user/conf/config.php
+++ SAMPLE/user/conf/config.php
@@ -1,10 +1,15 @@
 <?php
-    $dbuser="sql12780355";
-    $dbpass="";
-    $host="host.com";
-    $db="sql12780355";
-    $mysqli=new mysqli($host,$dbuser, $dbpass, $db);
+$host = "sql12.freesqldatabase.com";     // remote host
+$user = "sql12780355";                   // your DB user
+$pass = "Pgly8MQ1dN";                         // your DB password
+$db   = "sql12780355";                   // your DB name
+$port = 3306;                           // port number (optional, default is 3306)
 
-  
+$conn = new mysqli($host, $user, $pass, $db, $port);
+if ($conn->connect_error) {
+    die(json_encode(["success" => false, "message" => "Database connection failed."]));
+}
 
-    
+date_default_timezone_set('Asia/Kolkata'); // Set timezone to IST (Indian Standard Time)
+$error = "";
+?>
