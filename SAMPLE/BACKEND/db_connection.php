291ce696782e316472a867fe090d69a76d959a0b
Mohan Reddy <129178102+ComradeMohan@users.noreply.github.com>
2025-05-21 19:08:21 +0530

    Merge branch 'main' of https://github.com/ComradeMohan/pace-bank

--- SAMPLE/BACKEND/db_connection.php
+++ SAMPLE/BACKEND/db_connection.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 header('Content-Type: application/json');
 
 $host = 'host.com';
@@ -7,12 +8,19 @@ $password = '';
 $db_name = 'banking';
 
 $conn = new mysqli($host, $user, $password, $db_name);
+=======
+$host = "sql12.freesqldatabase.com";     // remote host
+$user = "sql12780355";                   // your DB user
+$pass = "Pgly8MQ1dN";                         // your DB password
+$db   = "sql12780355";                   // your DB name
+$port = 3306;                           // port number (optional, default is 3306)
+>>>>>>> 06d10dc81a793da7867f238805c7d2b705930639
 
+$conn = new mysqli($host, $user, $pass, $db, $port);
 if ($conn->connect_error) {
-    die(json_encode([
-        "status" => false,
-        "message" => "Database connection failed: " . $conn->connect_error,
-        "data" => []
-    ]));
+    die(json_encode(["success" => false, "message" => "Database connection failed."]));
 }
+
+date_default_timezone_set('Asia/Kolkata'); // Set timezone to IST (Indian Standard Time)
+$error = "";
 ?>
--- db.php
+++ db.php
@@ -1,9 +1,15 @@
 <?php
-$mysqli = mysqli_connect("host.com", "sql12780355", "", "sql12780355");
-if (mysqli_connect_errno()) {
-    echo "Failed to connect to MySQL: " . mysqli_connect_error();
-    die();
+$host = "sql12.freesqldatabase.com";     // remote host
+$user = "sql12780355";                   // your DB user
+$pass = "Pgly8MQ1dN";                         // your DB password
+$db   = "sql12780355";                   // your DB name
+$port = 3306;                           // port number (optional, default is 3306)
+
+$conn = new mysqli($host, $user, $pass, $db, $port);
+if ($conn->connect_error) {
+    die(json_encode(["success" => false, "message" => "Database connection failed."]));
 }
 
 date_default_timezone_set('Asia/Kolkata'); // Set timezone to IST (Indian Standard Time)
 $error = "";
+?>
